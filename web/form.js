b1f708016305db6592dc83d14bec0e71ef05d4d6
Isabela Silva <isabelasilvas16@gmail.com>
2023-09-14 12:42:10 -0300

    js link youtube

---
+++ web/form.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("#form")
+const input = document.querySelector("#url")
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+
+  const videoURL = input.value
+  console.log("URL DO VÍDEO: ", videoURL)
+})
