0674b85431889ff40200050105dcf7a99976c819
Isabela Silva <isabelasilvas16@gmail.com>
2023-09-13 21:18:26 -0300

    adicionando link short do youtube

--- server/download.js
+++ server/download.js
@@ -2,5 +2,8 @@ import ytdl from "ytdl-core"
 import fs from "fs"
 
 export const download = (videoId) => {
+  const videoURL = "https://www.youtube.com/shorts/" + videoId
   console.log("Realizado o download do vídeo:", videoId)
+
+  ytdl()
 }
