0bac2ac56ab33865c733df3b7fa338b0585a7661
Isabela Silva <isabelasilvas16@gmail.com>
2023-09-14 13:12:07 -0300

    att js

--- web/form.js
+++ web/form.js
@@ -1,8 +1,10 @@
+import { server } from "./server.js"
+
 const form = document.querySelector("#form")
 const input = document.querySelector("#url")
 const content = document.querySelector("#content")
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault()
 
   const videoURL = input.value
@@ -15,4 +17,8 @@ form.addEventListener("submit", (event) => {
   const [videoID] = params.split("?si")
 
   content.textContent = "Obtendo o texto do áudio..."
+
+  const transcription = await server.get("/summary/" + videoID)
+
+  constent.textContent = transcription.data.result
 })
