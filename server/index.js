bd48525d4d3bf1dc5e83270a00cbd7798fdea650
Isabela Silva <isabelasilvas16@gmail.com>
2023-09-13 21:06:06 -0300

    adicionando id

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ import express from "express"
 const app = express()
 app.use(cors())
 
-app.get("/summary", (request, response) => {
-  response.send("OK, vou fazer o resumo")
+app.get("/summary/:id", (request, response) => {
+  response.send("ID do vídeo:" + request.params.id)
 })
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
