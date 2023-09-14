1959a346ba907bd6a0d404ff526938bdf2b9cb53
Isabela Silva <isabelasilvas16@gmail.com>
2023-09-13 21:49:28 -0300

    informação que o vídeo terminou de fazer download

--- server/download.js
+++ server/download.js
@@ -5,14 +5,15 @@ export const download = (videoId) => {
   const videoURL = "https://www.youtube.com/shorts/" + videoId
   console.log("Realizado o download do vídeo:", videoId)
 
-  ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" }).on(
-    "info",
-    (info) => {
+  ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
+    .on("info", (info) => {
       const seconds = info.formats[0].approxDurationMs / 1000
 
       if (seconds > 60) {
         throw new Error("A duração desse vídeo é mais que 60 segundos.")
       }
-    }
-  )
+    })
+    .on("end", () => {
+      console.log("Download do vídeo dinalizado.")
+    })
 }
